730a1ea741750c5455fe89c8526d16e8cc855fa9
Siphamandla Ngwenya <ngwenyaspa@gmail.com>
2023-04-25 21:46:07 +0200

    Update function2.c

--- function2.c
+++ function2.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /****************** PRINT POINTER ******************/
 /**
+ * Team Project Siphamandla by Getrude
  * print_pointer - Prints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
